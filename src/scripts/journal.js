d86ed7760d574f592b057eed215a79adb01c3bab
AliAbdulle <alidhobey@gmail.com>
2019-03-01 20:44:18 -0600

    added journal.js

---
+++ src/scripts/journal.js
@@ -0,0 +1,14 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const objectsJournalEntry = {
+    journalData: "03/01/2019",
+    covered: "JavaScript object",
+    journal: "Learn JavaScript object",
+    mod: "Ok"
+
+}
+
+addListJournal = []
+addListJournal.push(objectsJournalEntry)
