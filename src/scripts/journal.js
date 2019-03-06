1bc79edf246c1e9e6af486694d0c4069190ea16f
AliAbdulle <alidhobey@gmail.com>
2019-03-05 18:07:29 -0600

    deleted typescript and changed layout

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,13 +2,21 @@
     Define the keys and value for a JavaScript object that
     represents a journal entry about what you learned today
 */
-const objectsJournalEntry = {
+const journalEntry = {
     journalData: "03/01/2019",
     covered: "JavaScript object",
     journal: "Learn JavaScript object",
     mod: "Ok"
 
 }
+const journalEntry1 = {
+    journalData: "08/01/2019",
+    covered: "JavaScript looping",
+    journal: "Learn JavaScript looping",
+    mod: "it okay but i get it"
+
+}
 
 addListJournal = []
-addListJournal.push(objectsJournalEntry)
+addListJournal.push(journalEntry, journalEntry1)
+console.log(addListJournal)
